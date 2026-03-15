8d2412877b62ca31a887f7e2a3c429a1fd617f6c
Lukas166 <lukasaustin16@gmail.com>
2026-03-15 19:47:57 +0700

    chore: reformat code by prettier

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -4,7 +4,7 @@ import { prismaAdapter } from "better-auth/adapters/prisma";
 
 import prisma from "./prisma";
 export const auth = betterAuth({
-    database: prismaAdapter(prisma, {
-        provider: "postgresql",
-    }),
+  database: prismaAdapter(prisma, {
+    provider: "postgresql",
+  }),
 });
